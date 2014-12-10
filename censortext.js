2b6880a8773739e372ed9fc4d842cc7e2b17ea8f
johncox00 <john@mach19.com>
2014-12-10 04:53:37 -0700

    first commit

---
+++ censortext.js
@@ -0,0 +1,20 @@
+var censorWords = ["sad", "bad", "mad"];
+var customCensoredWords = [];
+function censor(inStr) {
+  for (idx in censorWords) {
+    inStr = inStr.replace(censorWords[idx], "*****");
+  }
+  for (idx in customCensoredWords){
+    inStr = inStr.replace(customCensoredWords[idx], "*****");
+  }
+  return inStr;
+}
+function addCensoredWord(word){
+  customCensoredWords.push(word);
+}
+function getCensoredWords(){
+  return censorWords.concat(customCensoredWords);
+}
+exports.censor = censor;
+exports.addCensoredWord = addCensoredWord;
+exports.getCensoredWords = getCensoredWords;
